5cb2943b70b3a4bc55960ce9bb43be0b5d580dc8
m-slezak <77272905+m-slezak@users.noreply.github.com>
2022-10-14 14:55:34 +0200

    weatger fetch

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?q=Warsaw&appid=51a63434bef5e5bc4aaaaa394f7c0090";
 
 const Weather = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  console.log(data);
+
+  if (!data) return null;
+
   return (
     <div>
-         <p>Dallas</p>
-         <p>22°</p>
+      <p>{data.main.name}</p>
+      <p>{data.main.temp}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Weather
+export default Weather;
