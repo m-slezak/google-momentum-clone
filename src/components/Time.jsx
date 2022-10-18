bb2255c2b22b4716449f7e62aab2f752721c6203
m-slezak <77272905+m-slezak@users.noreply.github.com>
2022-10-18 08:33:42 +0200

    Time

--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -5,31 +5,24 @@ import "moment-timezone";
 const Time = () => {
   let date = new Date();
 
-  const greeting = () =>{
-    if (date.getHours() >=5 && date.getHours() < 11){
-        return "Good Morning, Michał"
+  const greeting = () => {
+    if (date.getHours() >= 5 && date.getHours() < 11) {
+      return "Good Morning, Michał";
+    } else if (date.getHours() >= 11 && date.getHours() < 17) {
+      return "Good Afternoon, Michał";
+    } else if (date.getHours() >= 17 && date.getHours() < 24) {
+      return "Good Evening, Michał";
+    } else {
+      return "What are you doing up at this hour?";
     }
-    else if (date.getHours() >=11 && date.getHours() <17){
-        return "Good afternoon, Michał"
-    }
-    else if (date.getHours() >=17 && date.getHours()<24 ){
-        return "Good evening, Michał")
-    }
-    else {
-        return "What the heck you doing so late?"
-    }
-  }
-
-  if (time.getHours() > 18 && time.getHours() <= 24)
-    console.log(;
-  else {
-    console.log();
-  }
+  };
 
   return (
     <div>
-      <p>time</p>
-      <p>Welcome Welcome Welcome Welcome Welcome</p>
+      <p>
+        <Moment format="LT"></Moment>
+      </p>
+      <p>{greeting()}</p>
     </div>
   );
 };
